a154b68e7ff155b1f4f870c1241666753ba8b212
Geoff Jarman <geoffxp@gmail.com>
2021-06-10 10:13:14 -0500

    Updates

---
+++ precache-manifest.392d5256456a4f1c79abe4fc7657324e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b1b7e1d66237f567cf8b915ccc9131",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "8de11268c77f071df89e",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro-timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de11268c77f071df89e",
+    "url": "/pomodoro-timer/static/js/main.f4466441.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
